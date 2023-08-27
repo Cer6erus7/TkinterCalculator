aa779f14e4b630854c715d4e93fd2ff0cec3174c
Roker <matvey14481@gmail.com>
2023-08-26 21:39:22 -0700

    Finall version of this calculator!

--- main.py
+++ main.py
@@ -1,4 +1,61 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+def calculating():
+    value = enter.get()
+    if value[-1] in "-+/*":
+        value = value[:-1] + value[-1] + value[:-1]
+    enter.delete(0, tk.END)
+    try:
+        enter.insert(tk.END, eval(value))
+    except NameError:
+        messagebox.showinfo("Error!", "Incorrect input, try again!")
+        remover()
+    except SyntaxError:
+        messagebox.showinfo("Error!", "Syntax error, try again!")
+        remover()
+    except ZeroDivisionError:
+        messagebox.showinfo("Error!", "You cannot divide zeroes!")
+        remover()
+
+
+def add_number(number):
+    if enter.get()[0] == "0" and len(enter.get()) == 1:
+        enter.delete(0)
+    enter.insert(tk.END, number)
+
+
+def btn_number(symbol):
+    return tk.Button(win, text=symbol, command=lambda: add_number(symbol), bd=4)
+
+
+def add_operator(operator):
+    value = enter.get()
+    if value[-1] in '*-/+':
+        enter.delete(len(value) - 1)
+    if '+' in value or '-' in value or '/' in value or '*' in value:
+        calculating()
+    enter.insert(tk.END, operator)
+
+
+def btn_operator(symbol):
+    return tk.Button(win, text=symbol, command=lambda: add_operator(symbol), bd=4)
+
+
+def remover():
+    enter.delete(0, tk.END)
+    enter.insert(tk.END, "0")
+
+
+def press_key(event):
+    print(event)
+    if event.char.isdigit():
+        add_number(event.char)
+    if event.char in "+/-*":
+        add_operator(event.char)
+    if event.char == "=" or event.keysym == "Return":
+        calculating()
 
 
 win = tk.Tk()
@@ -8,76 +65,32 @@ win.config(bg="#153023")
 win.resizable(False, False)
 logo = tk.PhotoImage(file='img.png')
 win.iconphoto(False, logo)
+win.bind("<Key>", press_key)
 
 #153023  green color
 #33ffe6  blue color
-
-
-def calculating():
-    value = enter.get()
-    enter.delete(0, tk.END)
-    try:
-        if value[-1] in "-+/*":
-            enter.insert(0, eval(value[:-1] + value[-1] + value[:-1]))
-        else:
-            enter.insert(tk.END, eval(value))
-    except NameError:
-        enter.insert(tk.END, "Incorrect input, try again!")
-    except SyntaxError:
-        enter.insert(tk.END, "Syntax error, try again!")
-    except ZeroDivisionError:
-        enter.insert(tk.END, "You cannot divide zeroes!")
-
-
-def post_number(number):
-    if enter.get()[0] == "0" and len(enter.get()) == 1:
-        enter.delete(0)
-    enter.insert(tk.END, number)
-
-
-def make_number(symbol):
-    return tk.Button(win, text=symbol, command=lambda: post_number(symbol), bd=4)
-
-
-def post_operator(operator):
-    value = enter.get()
-    if value[-1] in '*-/+':
-        enter.delete(len(value) - 1)
-    if '+' in value or '-' in value or '/' in value or '*' in value:
-        calculating()
-    enter.insert(tk.END, operator)
-
-
-def make_operator(symbol):
-    return tk.Button(win, text=symbol, command=lambda: post_operator(symbol), bd=4)
-
-
-def remover():
-    enter.delete(0, tk.END)
-    enter.insert(tk.END, "0")
-
 
 enter = tk.Entry(win, borderwidth=4, relief=tk.RAISED, justify=tk.RIGHT, font=("Arial", 15))
 enter.grid(row=0, columnspan=4, stick='wens', pady=5, padx=5)
 enter.insert(tk.END, "0")
 
-make_number("1").grid(row=1, column=0, stick='wens', pady=5, padx=5)
-make_number("2").grid(row=1, column=1, stick='wens', pady=5, padx=5)
-make_number("3").grid(row=1, column=2, stick='wens', pady=5, padx=5)
-make_number("4").grid(row=2, column=0, stick='wens', pady=5, padx=5)
-make_number("5").grid(row=2, column=1, stick='wens', pady=5, padx=5)
-make_number("6").grid(row=2, column=2, stick='wens', pady=5, padx=5)
-make_number("7").grid(row=3, column=0, stick='wens', pady=5, padx=5)
-make_number("8").grid(row=3, column=1, stick='wens', pady=5, padx=5)
-make_number("9").grid(row=3, column=2, stick='wens', pady=5, padx=5)
-make_number("0").grid(row=4, column=1, stick='wens', pady=5, padx=5)
+btn_number("1").grid(row=1, column=0, stick='wens', pady=5, padx=5)
+btn_number("2").grid(row=1, column=1, stick='wens', pady=5, padx=5)
+btn_number("3").grid(row=1, column=2, stick='wens', pady=5, padx=5)
+btn_number("4").grid(row=2, column=0, stick='wens', pady=5, padx=5)
+btn_number("5").grid(row=2, column=1, stick='wens', pady=5, padx=5)
+btn_number("6").grid(row=2, column=2, stick='wens', pady=5, padx=5)
+btn_number("7").grid(row=3, column=0, stick='wens', pady=5, padx=5)
+btn_number("8").grid(row=3, column=1, stick='wens', pady=5, padx=5)
+btn_number("9").grid(row=3, column=2, stick='wens', pady=5, padx=5)
+btn_number("0").grid(row=4, column=1, stick='wens', pady=5, padx=5)
 
 tk.Button(win, text="C", command=remover, bd=4).grid(row=4, column=0, stick='wens', pady=5, padx=5)
 
-make_operator("+").grid(row=1, column=3, stick='wens', pady=5, padx=5)
-make_operator("-").grid(row=2, column=3, stick='wens', pady=5, padx=5)
-make_operator("/").grid(row=3, column=3, stick='wens', pady=5, padx=5)
-make_operator("*").grid(row=4, column=3, stick='wens', pady=5, padx=5)
+btn_operator("+").grid(row=1, column=3, stick='wens', pady=5, padx=5)
+btn_operator("-").grid(row=2, column=3, stick='wens', pady=5, padx=5)
+btn_operator("/").grid(row=3, column=3, stick='wens', pady=5, padx=5)
+btn_operator("*").grid(row=4, column=3, stick='wens', pady=5, padx=5)
 tk.Button(win, text="=", command=calculating, bd=4).grid(row=4, column=2, stick='wens', pady=5, padx=5)
 
 
